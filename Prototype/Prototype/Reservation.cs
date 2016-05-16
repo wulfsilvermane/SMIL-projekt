f5c2b0eafb9a0425230009ff9935aa7043b5f81b
Martin Rettig Jensen <martin.silvermane@gmail.com>
2016-05-16 12:20:00 +0200

    Vidre arbejde med at ufylde begyndende klasser, og prelim design til start menu

--- Prototype/Prototype/Reservation.cs
+++ Prototype/Prototype/Reservation.cs
@@ -8,5 +8,31 @@ namespace Prototype
 {
     class Reservation
     {
+        enum Status {Reserveret, Tjekketind, Faerdig, Annulleret}   //Annulleret bruges til at sørge for at tiden bliver ledig, da der ved ikke fremmøde stadig bliver trukket nogen penge.
+                                                                    //Reserveret bruges som default. Når en reservation er bestilt, ligger den i databasen. Er den ikke bestilt, ligger den kun i klienten
+        private Status _status;
+        private int id;
+        private Patient _patient;
+        private Ansat _laege;
+        private Lokale _lokale;
+        private string beskrivelse; //Noter omkring reservationen
+        private DateTime _starttid;
+        private int varighed;   //Antal ½ timer som reservationen dækker; Tandlæger skema lægger i ½ timer.
+        public Reservation(int id)
+        {
+            this.id = id;
+        }
+        public void SetLaege(Ansat _laege)
+        {
+            this._laege = _laege;
+        }
+        public void SetLokale(Lokale _lokale)
+        {
+            this._lokale = _lokale;
+        }
+        public void SetPatient(Patient _patient)
+        {
+            this._patient = _patient;
+        }
     }
 }
