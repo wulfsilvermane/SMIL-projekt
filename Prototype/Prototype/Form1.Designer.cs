1e1772c5723e3de194a6453c9690866295165679
Younes Zakaria <drcooldudealt@gmail.com>
2016-05-19 13:34:18 +0200

    Finpudser Form1 design :)

--- Prototype/Prototype/Form1.Designer.cs
+++ Prototype/Prototype/Form1.Designer.cs
@@ -35,9 +35,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(12, 14);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 40);
+            this.button1.Size = new System.Drawing.Size(148, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "Patienter";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,18 +45,18 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 58);
+            this.button2.Location = new System.Drawing.Point(12, 65);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.Size = new System.Drawing.Size(148, 40);
             this.button2.TabIndex = 1;
             this.button2.Text = "Reservationer";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 104);
+            this.button3.Location = new System.Drawing.Point(12, 115);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 40);
+            this.button3.Size = new System.Drawing.Size(148, 40);
             this.button3.TabIndex = 2;
             this.button3.Text = "Administration <N/A>";
             this.button3.UseVisualStyleBackColor = true;
@@ -65,14 +65,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(132, 169);
+            this.ClientSize = new System.Drawing.Size(172, 169);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(140, 196);
+            this.MaximumSize = new System.Drawing.Size(180, 196);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(140, 196);
+            this.MinimumSize = new System.Drawing.Size(180, 196);
             this.Name = "StartMenu";
             this.Text = "Start";
             this.ResumeLayout(false);
